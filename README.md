267cfa73e8edafbb5af227e3086a43fb9a8c729c
Adam Wilczyński <a9c2xx@gmail.com>
2019-07-23 23:23:27 +0200

    Add possibility of making elements darker.

---
+++ README.md
@@ -0,0 +1,12 @@
+<h1>Etch A Sketch</h1>
+<p>Created for The Odin Project Web Development 101 course.<p>
+<p>Made by A9C2 using HTML, CSS and JavaScript.</p>
+
+<h2>Instructions</h2>
+<p>Move your mouse on the white grid to paint it's elements with random RGB color.</p>
+<p>Hover your mouse on the same element to make it darker.</p>
+
+<h3>Used resources:</h3>
+<ul>
+    <li>Restart button icon: https://icons8.com/icon/1457/restart</li>
+</ul>
--- code.js
+++ code.js
@@ -1,17 +1,52 @@
-function createGrid () {
-    for (i = 0; i < 16; i++) {
+const restartButton = document.querySelector("#restartButton");
+restartButton.addEventListener("click", restart)
+
+function restart() {
+    window.location.reload(true);
+}
+
+function createGrid() {
+    let gridWidth = parseInt(prompt("What would you like the width to be? [max 100]"), 10);
+    if (gridWidth > 100) gridWidth = 100;
+    for (i = 0; i < (gridWidth * gridWidth); i++) {
         var div = document.createElement("DIV");
         div.classList.add("box");
-        div.addEventListener("click", paint)
+        div.style.width = `${840 / gridWidth}px`;
+        div.style.height = `${840 / gridWidth}px`;
+        div.addEventListener("mouseenter", paint);
         document.body.querySelector("#grid").appendChild(div);
     }
-    
+
     function paint(e) {
-        e.target.classList.add("painted");
+        if (e.target.style.backgroundColor == "") {
+            e.target.style.backgroundColor = getRandomRGB();
+        } else {
+            console.log(e.target.style.filter);
+            switch (e.target.style.filter) {
+                case "":
+                    e.target.style.filter = "brightness(80%)";
+                    break;
+                case "brightness(80%)":
+                    e.target.style.filter = "brightness(60%)";
+                    break;
+                case "brightness(60%)":
+                    e.target.style.filter = "brightness(40%)";
+                    break;
+                case "brightness(40%)":
+                    e.target.style.filter = "brightness(20%)";
+                    break;
+                case "brightness(20%)":
+                    e.target.style.filter = "brightness(0%)";
+                    break;
+            }
+        }
     }
 }
 
-const restart = document.querySelector("#restart");
-
-
+function getRandomRGB() {
+    r = Math.floor(Math.random() * 256);
+    g = Math.floor(Math.random() * 256);
+    b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`
+}
 createGrid();
