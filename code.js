978eba58d4778c8b6ecd31ceacc679d5f8c23231
Adam Wilczyński <a9c2xx@gmail.com>
2019-07-23 13:51:14 +0200

    Add grid create function

--- code.js
+++ code.js
@@ -1,12 +1,17 @@
-
-for (i = 0; i < 16; i++) {
-    var div = document.createElement("DIV");
-    div.classList.add("box");
-    div.addEventListener("click", paint)
-    document.body.querySelector("#grid").appendChild(div);
+function createGrid () {
+    for (i = 0; i < 16; i++) {
+        var div = document.createElement("DIV");
+        div.classList.add("box");
+        div.addEventListener("click", paint)
+        document.body.querySelector("#grid").appendChild(div);
+    }
+    
+    function paint(e) {
+        e.target.classList.add("painted");
+    }
 }
 
-function paint(e) {
-    e.target.classList.add("painted");
-}
+const restart = document.querySelector("#restart");
 
+
+createGrid();
